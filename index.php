5e096c262bb99f7c5849e705aa2a894b6c4a32f0
Mattias Geniar <m@ttias.be>
2016-01-22 20:34:10 +0100

    fix the display: uppercase the words properly, show dashes instead of PHP's underscores

--- index.php
+++ index.php
@@ -4,7 +4,16 @@
 /* All $_SERVER variables prefixed with HTTP_ are the HTTP headers */
 foreach ($_SERVER as $header => $value) {
   if (substr($header, 0, 5) == 'HTTP_') {
-    $clean_header = substr($header, 5, strlen($header));
+    /* Strip the HTTP_ prefix from the $_SERVER variable, what remains is the header */
+    $clean_header = strtolower(substr($header, 5, strlen($header)));
+
+    /* Replace underscores by the dashes, as the browser sends them */
+    $clean_header = str_replace('_', '-', $clean_header);
+
+    /* Cleanup: standard headers are first-letter uppercase */
+    $clean_header = ucwords($clean_header, " \t\r\n\f\v-");
+
+    /* And show'm */
     echo '<strong>'. $clean_header .'</strong>: '. $value .'<br />';
   }
 }
